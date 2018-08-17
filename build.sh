207fc7ba2ab5a41ca429fb36a250e9cc89024ba6
Michael Wyraz <michael@wyraz.de>
2018-08-17 11:06:30 +0200

    Some progress

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -e
+
+export PACMAN_VERSION=5.1.1
+
 rm -rf work
 mkdir work
 mkdir work/rootfs
 (cd work && curl -s https://git.archlinux.org/arch-install-scripts.git/snapshot/arch-install-scripts-18.tar.gz | tar xfvz -)
 (cd work/arch-install-scripts-18 && make)
-work/arch-install-scripts-18/pacstrap work/rootfs \
+(cd work && curl -s https://sources.archlinux.org/other/pacman/pacman-${PACMAN_VERSION}.tar.gz | tar xfvz -)
+(cd work/pacman-${PACMAN_VERSION} && ./configure --prefix=/)
+(cd work/pacman-${PACMAN_VERSION} && make && make install)
+(cd work/pacman-${PACMAN_VERSION}/src/pacman && make && make install)
+
+cp pacman.conf /etc/pacman.conf
+mkdir -p /etc/pacman.d/
+echo 'Server = http://mirrors.kernel.org/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+
+pacman -Syu --noconfirm -r work/rootfs pacman \
   filesystem systemd-sysvcompat bash bzip2 coreutils \
   diffutils file findutils gawk gcc-libs gettext glibc \
   grep gzip inetutils iproute2 less licenses logrotate \
